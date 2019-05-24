24eaa911b2f17702cb3be310addda24cb6a84134
Neil Weidinger <neilweidingern@gmail.com>
2019-05-25 00:43:25 +0200

    decent prototype imo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
+import Container from 'react-bootstrap/Container';
+import Header from './Header';
 
 function App(props) {
     return (
-        <h1>Base-station GUI</h1>
+        <Container>
+            <Header /> 
+            <h1>Base-station GUI</h1>
+        </Container>
     );
 }
 
