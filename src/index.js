0339843c3b482fc4fe528b554141f3bc1f6d285a
Neil Weidinger <neilweidingern@gmail.com>
2019-05-25 22:07:53 +0200

    added motor gauges

--- src/index.js
+++ src/index.js
@@ -10,18 +10,32 @@ function App(props) {
     return (
         <div className="wrapper">
             <Header /> 
-            <div id="gauges">
+            <div id="gauges-1">
                 <div id="velocity_gauge">
                     <Gauge
                         rotate='100'
-                        size='150'
+                        size='180'
                         title='VELOCITY'
                     />
                 </div>
                 <Gauge
                     rotate='60'
-                    size='100'
+                    size='120'
                     title='ACCELERATION'
+                />
+            </div>
+            <div id="gauges-2">
+                <div id="motor_left_gauge">
+                    <Gauge
+                        rotate='100'
+                        size='80'
+                        title='LEFT MOTOR RPM'
+                    />
+                </div>
+                <Gauge
+                    rotate='60'
+                    size='80'
+                    title='RIGHT MOTOR RPM'
                 />
             </div>
         </div>
