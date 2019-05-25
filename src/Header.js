cf6b7b77d843af2efacdf502ad8fd3f307833f03
Neil Weidinger <neilweidingern@gmail.com>
2019-05-25 02:18:43 +0200

    Stopped using bootstrap layouts and using Grid/Flexbox instead

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import logo from './hyped.png';
 
@@ -9,17 +7,11 @@ function Header(props) {
     const podProgress = 90;
 
     return (
-        <Row style={{alignItems: 'center', marginTop: '20px'}}>
-            <Col md={3}>
-                <img src={logo} className="img-fluid" alt="HypED logo"/>
-            </Col>
-            <Col>
-                <ProgressBar animated now={podProgress} label={`${podProgress} m`} />
-            </Col>
-            <Col md={2}>
-                <Button style={{float: 'right'}} variant="primary">STATUS</Button>
-            </Col>
-        </Row>
+        <div className="header">
+            <img id="logo" src={logo} alt="HypED logo"/>
+            <ProgressBar id="pod_progress" now={podProgress} label={`${podProgress} m`} />
+            <Button id="pod_status" variant="primary">STATUS</Button>
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Container from 'react-bootstrap/Container';
 import Header from './Header';
 
 function App(props) {
     return (
-        <Container>
+        <div className="wrapper">
             <Header /> 
             <h1>Base-station GUI</h1>
-        </Container>
+        </div>
     );
 }
 
