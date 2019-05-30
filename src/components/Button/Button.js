08cc27c9a7e4445839f96e4585de298a0404f87d
Neil Weidinger <neilweidingern@gmail.com>
2019-05-30 23:56:51 +0200

    buttons disabled conditionally depending on pod state, only wrote code for a few pod states, still need to finish

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,21 +2,12 @@ import './Button.css';
 
 import React from 'react';
 
-class Button extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            disabled: false,
-        };
-    }
-
-    render() {
-        return (
-            <button disabled={this.state.disabled} className="button-root" onClick={this.props.handleClick}>
-                {this.props.name}
-            </button>
-        );
-    }
+function Button(props) {
+    return (
+        <button disabled={props.disabled} className="button-root" onClick={props.handleClick}>
+            {props.name}
+        </button>
+    );
 }
 
 export default Button
--- src/components/ButtonContainer/ButtonContainer.js
+++ src/components/ButtonContainer/ButtonContainer.js
@@ -17,39 +17,97 @@ class ButtonContainer extends React.Component {
     }
 
     render() {
+        const disabledButtons = determineDisabledButtons(this.props.state);
+
         return (
             <div className="button-container-root">
                 <div className="button-container-button">
                     <Button
-                        name='SVP'
+                        name='SVP GO'
+                        disabled={disabledButtons.svpGo}
+                        handleClick={() => this.sendMessage({command: 'SERVICE_PROPULSION_GO'})}
+                    />
+                </div>
+                <div className="button-container-button">
+                    <Button
+                        name='SVP STOP'
+                        disabled={disabledButtons.svpStop}
+                        handleClick={() => this.sendMessage({command: 'SERVICE_PROPULSION_STOP'})}
                     />
                 </div>
                 <div className="button-container-button">
                     <Button
                         name='CALIB.'
+                        disabled={disabledButtons.calibrate}
                         handleClick={() => this.sendMessage({command: 'CALIBRATE'})}
                     />
                 </div>
                 <div className="button-container-button">
                     <Button
                         name='RESET'
+                        disabled={disabledButtons.reset}
                         handleClick={() => this.sendMessage({command: 'RESET'})}
                     />
                 </div>
                 <div className="button-container-button">
                     <Button
                         name='LAUNCH'
+                        disabled={disabledButtons.launch}
                         handleClick={() => this.sendMessage({command: 'LAUNCH'})}
                     />
                 </div>
                 <div className="button-container-button">
                     <Button
                         name='STOP'
+                        disabled={disabledButtons.stop}
                         handleClick={() => this.sendMessage({command: 'STOP'})}
                     />
                 </div>
             </div>
         );
+    }
+}
+
+// returns object indicating if certain button is disabled or not
+function determineDisabledButtons(state) {
+    switch(state) {
+        case 'IDLE':
+            return {
+                svpGo: true,
+                svpStop: true,
+                calibrate: false,
+                reset: true,
+                launch: true,
+                stop: true,
+            };
+        case 'CALIBRATING':
+            return {
+                svpGo: true,
+                svpStop: true,
+                calibrate: true,
+                reset: true,
+                launch: true,
+                stop: true,
+            };
+        case 'READY':
+            return {
+                svpGo: true,
+                svpStop: true,
+                calibrate: true,
+                reset: true,
+                launch: false,
+                stop: true,
+            }
+        default:
+            console.error("Error: received invalid state when trying to decide if button is disabled or not, probably not connected")
+            return {
+                svpGo: true,
+                svpStop: true,
+                calibrate: true,
+                reset: true,
+                launch: true,
+                stop: true,
+            };
     }
 }
 
