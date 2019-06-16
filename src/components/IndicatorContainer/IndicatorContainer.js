55d07eb78363b8a23367aec752f07cd43dd8dfe1
jmsridley <jmsridley@gmail.com>
2019-06-16 13:08:25 +0100

    fix IndicatorContainer

--- src/components/IndicatorContainer/IndicatorContainer.js
+++ src/components/IndicatorContainer/IndicatorContainer.js
@@ -4,13 +4,14 @@ import React from 'react';
 import Indicator from '../Indicator/Indicator';
 
 function IndicatorContainer(props) {
-    const indicators = Array.from(props.indicators, indicator => <div key={indicator.indicatorTitle} className="indicator-container-flex-item">
-                                                        <Indicator
-                                                            name={indicator.indicatorName}
-                                                            enabled={indicator.enabled}
-                                                        />
-                                                    </div>
-                            );
+    const indicators = Array.from(props.indicators, indicator => 
+                            <Indicator 
+                                key={indicator.indicatorName} 
+                                name={indicator.indicatorName}
+                                name={indicator.indicatorName}
+                                enabled={indicator.enabled}
+                            />
+                        );
     return (
         <div className="indicator-container-root">
             <h1 className="indicator-container-title">
