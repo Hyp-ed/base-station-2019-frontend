514d4e4efec6bba452308db4b43fadafebbb12ab
Neil Weidinger <neilweidingern@gmail.com>
2019-06-17 22:23:18 +0200

    indicators start as grey if they have not yet received any data

--- src/components/Indicator/Indicator.js
+++ src/components/Indicator/Indicator.js
@@ -3,7 +3,10 @@ import './Indicator.css'
 import React from 'react';
 
 function Indicator(props) {
-    const backgroundClassName = props.operational ? 'operational' : 'not-operational'
+    let backgroundClassName = '';
+    if (typeof props.operational !== 'undefined') {
+        backgroundClassName = props.operational ? 'operational' : 'not-operational';
+    }
 
     return (
         <div className='indicator-root'>
@@ -11,10 +14,6 @@ function Indicator(props) {
             <h2 className="indicator-name">{props.name}</h2>
         </div>
     );
-}
-
-Indicator.defaultProps = {
-    operational: true,  // TODO: Maybe change to some kinda non-initialized status?
 }
 
 export default Indicator;
