b5ccd7b0e21bd53bc00ea3ea440d6595344a9348
Neil Weidinger <neilweidingern@gmail.com>
2019-06-20 20:37:08 +0200

    moved timer to React component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,21 +2,7 @@ import './Header.css';
 
 import React from 'react';
 import logo from '../../hyped.png';
-
-//starts timer when called
-function startTimer(display) {
-    var seconds = 0;
-    setInterval(function () {
-        display.textContent = seconds + 's';
-        seconds++;
-    }, 1000);
-}
-
-//starts timer immediately (for testing)
-window.onload = function () {
-    var display = document.querySelector('#timer');
-    startTimer(display);
-};
+import Timer from '../Timer/Timer';
 
 function Header(props) {
     const podProgress = 90;
@@ -24,22 +10,14 @@ function Header(props) {
     const connectionTextClassName = props.connectedToPod ? 'connected' : 'not-connected';
     const connectionTextStyle = props.connectedToPod ? {marginBottom: -5} : {};
     const podStatusTextStyle = props.connectedToPod ? {} : {display: 'none'};
-    
-    var display = document.querySelector('#timer');
-
-    //continuously checks if state is accelerating
-    //if true, stops checking
-        if (props.podState === "ACCELERATING") {
-            startTimer(display);
-        }
 
     return (
         <div className="header">
             <img id="logo" src={logo} alt="HypED logo"/>
             <div className='progress-bar'></div>
-            <div className={`timer-wrapper`}>
-                <span id="timer">-s</span>
-            </div>
+            <Timer
+                podState={props.podState}
+            />
             <div className="pod-status">
                 <p className={`status-text connection-text ${connectionTextClassName}`} style={connectionTextStyle}>
                     {props.connectedToPod ? 'CONNECTED' : 'NOT CONNECTED'}
@@ -49,6 +27,5 @@ function Header(props) {
         </div>
     );
 }
-
 
 export default Header;
