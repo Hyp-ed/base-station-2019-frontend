f565a7fe168c3d4fb1ed7e164b9967ca91007a33
Neil Weidinger <neilweidingern@gmail.com>
2019-07-03 15:45:44 +0200

    filled in progress bar ends

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -3,14 +3,16 @@ import './ProgressBar.css';
 import React from 'react';
 
 function ProgressBar(props) {
-    const fillStyle = {
+    const indicatorStyle = {
         left: `${Math.min(100, Math.max(0, (props.podDistance / props.progressBarMax * 100)))}%` // min/max to clamp percentage num in [0, 100]`
     };
 
     return (
         <div className='progress-bar-root'>
+            <div className='progress-bar-fill fill-1'></div>
+            <div className='progress-bar-fill fill-2'></div>
             <div className='progress-bar-background'></div>
-            <div className='progress-bar-fill' style={fillStyle}></div>
+            <div className='progress-bar-indicator' style={indicatorStyle}></div>
         </div>
     );
 }
