3f5e2f92103e8017204e26e145984280b2e47c6f
Calum McM <calummcmeekin@me.com>
2019-07-12 13:30:08 -0700

    Added dark logo

--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -1,14 +1,21 @@
 import './Switch.css';
 import React from 'react';
+import Header from '../Header/Header';
 
 function Switch(props){
     document.addEventListener('DOMContentLoaded', function () {
         var checkbox = document.querySelector('input[type="checkbox"]');
+        var logoDark = document.getElementById('logoDark');
+        var logoLight = document.getElementById('logo');
         checkbox.addEventListener('change', function () {
           if (checkbox.checked) {
             document.documentElement.classList.toggle('light-mode');
+            logoDark.style.visibility = 'visible';
+            logoLight.style.visibility = 'hidden';
         } else {
             document.documentElement.classList.remove('light-mode');
+            logoDark.style.visibility = 'hidden';
+            logoLight.style.visibility = 'visible';
           }
         });
     });
