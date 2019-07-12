61b35c48f5bca560faec3a7d0b9a138d8a496c7c
Calum McM <calummcmeekin@me.com>
2019-07-12 13:06:34 -0700

    Changed embedded react text to grey

--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -1,11 +1,20 @@
 import './Switch.css';
 import React from 'react';
+import Bar from '../Bar/Bar'
 
 function Switch(props){
-
+    document.addEventListener('DOMContentLoaded', function () {
+        var checkbox = document.querySelector('input[type="checkbox"]');
+        checkbox.addEventListener('change', function () {
+          if (checkbox.checked) {
+            document.documentElement.classList.toggle('light-mode');
+        } else {
+            document.documentElement.classList.remove('light-mode');
+          }
+        });
+    });
     return(
         <div id="swith-box">
-            <h2>Mode: {props.title}</h2>
             <label class="switch">
                 <input type="checkbox"/>
                 <span class="slider"></span>
