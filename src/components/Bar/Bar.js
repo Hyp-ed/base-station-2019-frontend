11c2bdca977d1de77c78707408552da9b9be9111
Neil Weidinger <neilweidingern@gmail.com>
2019-07-12 16:28:51 -0700

    minor refactoring

--- src/components/Bar/Bar.js
+++ src/components/Bar/Bar.js
@@ -3,10 +3,8 @@ import './Bar.css';
 import React from 'react';
 
 function Bar(props) {
-    const indicatorStyle = {
-        width: `${Math.min(100,Math.max(0,
-            (props.value - props.min)/(props.max - props.min)*100
-        ))}%` // min/max to clamp percentage num in [0, 100]`
+    const barStyle = {
+        width: `${Math.min(100, Math.max(0, (props.value - props.min) / (props.max - props.min) * 100))}%` // min/max to clamp percentage num in [0, 100]`
     };
 
     return (
@@ -14,7 +12,7 @@ function Bar(props) {
             <h2 className="bar-name">{props.name}</h2>
             <div>
                 <div className="bar background">
-                    <div className="bar fill" style={indicatorStyle}></div>
+                    <div className="bar fill" style={barStyle}></div>
                 </div>
             </div>
             <div className="bar value">
