2eece32f564e7d8b916963bd6f26ec2d32179ae0
Neil Weidinger <neilweidingern@gmail.com>
2019-07-13 00:56:20 -0700

    basically got battery bars wired up in this commit

--- src/components/Bar/Bar.js
+++ src/components/Bar/Bar.js
@@ -7,35 +7,22 @@ function Bar(props) {
         width: `${Math.min(100, Math.max(0, (props.value - props.min) / (props.max - props.min) * 100))}%` // min/max to clamp percentage num in [0, 100]`
     }
 
-    const barColor = 
-        props.value > props.min 
-        ? "#ffffff"
-        : "#a31606";
-
-    const indicatorStyle = {
-        backgroundColor: `${barColor}`,
-        width: `${Math.min(100,Math.max(0,
-            props.value/props.max*100
-        ))}%` // min/max to clamp percentage num in [0, 100]`
-    };
-
-    const bufferStyle = {
-        width: `${(props.min/props.max)*100}%`
+    let barClassName = '';
+    if (props.value < props.min || props.value > props.max) {
+        barClassName = 'out-of-bounds';
     }
-    
+
     return (
         <div className="bar-root">
-            <h2 className="bar-name">{props.name}</h2>
-            <div>
-                <div className="bar background">
-                    <div className="bar fill" style={barStyle}></div>
-                    <div className="bar buffer" style={bufferStyle}></div>
-                </div>
+            <h2 className={`bar-name ${barClassName}`}>{props.name}</h2>
+            <div className="bar container">
+                <div className="bar background"></div>
+                <div className={`bar fill ${barClassName}`} style={barStyle}></div>
             </div>
             <div className="bar value">
-                <h2 className="bar-value-text">{props.value}</h2>
+                <h2 className={`bar-value-text ${barClassName}`}>{props.value}</h2>
                 <div className="bar-value-unit">
-                    <h2 className="bar-value-text">&nbsp;{props.unit}</h2>
+                    <h2 className={`bar-value-text ${barClassName}`}>&nbsp;{props.unit}</h2>
                 </div>
             </div>
         </div>
@@ -43,7 +30,7 @@ function Bar(props) {
 }
 
 Bar.defaultProps = {
-    value: 0,
+    value: '-',
 }
 
 export default Bar;
